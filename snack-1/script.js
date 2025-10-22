d830a6e0bd31d7420c75b02e63021b88a1cddba8
angelostori <angelostori13@gmail.com>
2025-10-22 15:21:26 +0200

    loop a list to print in console the list items

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,10 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+    console.log(name);
+    
+}
+
