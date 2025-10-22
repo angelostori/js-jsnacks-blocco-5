ab3cdecc1d2cba103f404506e673e629e6759530
angelostori <angelostori13@gmail.com>
2025-10-22 17:10:42 +0200

    search and log the class of student 'Marco Lanci'

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,11 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+let myClass;
+for (let i = 0; i < students.length; i++) {
+  const student = students[i];
+  if (student.name ==='Marco Lanci') {
+    myClass = student.class
+  }
+}
+console.log(myClass);
