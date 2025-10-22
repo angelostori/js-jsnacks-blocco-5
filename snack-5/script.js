a9146ec32e87bd2484c6292e2f2587c501d969e8
angelostori <angelostori13@gmail.com>
2025-10-22 16:12:44 +0200

    add comments to clarify even number filtering methods and use map function

--- snack-5/script.js
+++ snack-5/script.js
@@ -3,6 +3,7 @@ const nums = [2, 8, 4, 7, 12, 87];
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
 
+//for
 const evenNums = []
 for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
@@ -12,9 +13,20 @@ for (let i = 0; i < nums.length; i++) {
 }
 console.log(evenNums);
 
+//for each
 nums.forEach(num => {
     if (num % 2 === 0) {
         evenNums.push(num)
     }
 })
 console.log('forEach', evenNums);
+
+//map
+const evenNumsMap = nums.map((num) => {
+    if (num % 2 === 0) {
+        return num
+    }
+})
+console.log(evenNumsMap);
+
+   
